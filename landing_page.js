81c895190d908fdc55f5d85df34ab6b9e0e96dc1
Your Name <your-email@example.com>
2026-03-05 20:34:33 +0800

    Initial Changes

--- landing_page.js
+++ landing_page.js
@@ -2,20 +2,38 @@ const galleries = document.querySelectorAll(".product-gallery");
 
 galleries.forEach(gallery => {
 
-const mainImage = gallery.querySelector(".mainImage");
-const thumbs = gallery.querySelectorAll(".thumb");
+  const mainImage = gallery.querySelector(".mainImage");
+  const thumbs = gallery.querySelectorAll(".thumb");
 
-thumbs.forEach(thumb => {
+  thumbs.forEach(thumb => {
+    thumb.addEventListener("click", () => {
 
-thumb.addEventListener("click", ()=>{
+      // change the main image
+      mainImage.src = thumb.src;
 
-mainImage.src = thumb.src;
+      // remove active border from others
+      thumbs.forEach(t => t.classList.remove("active"));
 
-thumbs.forEach(t => t.classList.remove("active"));
-thumb.classList.add("active");
+      // add active to clicked
+      thumb.classList.add("active");
 
+    });
+  });
+//scrollbar
 });
 
+const scrollContainer = document.getElementById("productScroll");
+
+document.querySelector(".scroll-btn.left").addEventListener("click", () => {
+    scrollContainer.scrollBy({
+        left: -350,
+        behavior: "smooth"
+    });
 });
 
+document.querySelector(".scroll-btn.right").addEventListener("click", () => {
+    scrollContainer.scrollBy({
+        left: 350,
+        behavior: "smooth"
+    });
 });
